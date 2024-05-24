299c7fa72e344cd3ac66b0f0417933947fdefcc8
Alberto Polanco <japolancoc@hotmail.com>
2024-05-23 22:13:18 -0600

    Adición (temporal) de la api  de clima.

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,39 @@
 //Cantidad de noticias que se cargaran cada vez que se presione siguiente\
-let cantidadNoticias = 5;
+let cantidadNoticias = 9;
 let pageFinal = cantidadNoticias;
 let pageInicial = 0;
 let temaActual = "Tecnología";
 
+function imagenDelDia() {
+  fetch("http://localhost:3000/imagen/")
+    .then((response) => response.json())
+    .then(({ title, explanation, copyright, date, url, hdurl }) => {
+      const divImagen = document.getElementById("imagen");
+
+      const imagenDelDia = document.createElement("img");
+      imagenDelDia.src = url;
+      imagenDelDia.alt = title;
+
+      const divInformacion = document.createElement("div");
+      divInformacion.className = "informacion";
+
+      const informacion = document.createElement("h1");
+      informacion.innerHTML = title;
+
+      const explicacion = document.createElement("p");
+      explicacion.innerHTML = explanation;
+
+      divInformacion.appendChild(informacion);
+      divInformacion.appendChild(explicacion);
+
+      divImagen.appendChild(imagenDelDia);
+      divImagen.appendChild(divInformacion);
+    });
+}
+
 let noticias = {
   fetchNoticias: function (categoria) {
-    fetch(
-      "https://newsapi.org/v2/everything?q=" +
-        categoria +
-        "&languaje=es&apiKey=" +
-        this.apiKey
-    )
+    fetch(`http://localhost:3000/noticias/${categoria}`)
       .then((response) => response.json())
       .then((data) => this.displayNoticias(data));
   },
@@ -93,3 +115,98 @@ function siguiente() {
 }
 
 noticias.fetchNoticias(temaActual);
+imagenDelDia();
+
+window.addEventListener("load", () => {
+  let lon;
+  let lat;
+
+  let temperaturaValor = document.getElementById("temperatura-valor");
+  let temperaturaDescripcion = document.getElementById(
+    "temperatura-descripcion"
+  );
+  let ubicacion = document.getElementById("ubicacion");
+  let iconoAnimado = document.getElementById("icono-animado");
+  let vientoVelocidad = document.getElementById("viento-velocidad");
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((posicion) => {
+      console.log(posicion);
+      lon = posicion.coords.longitude;
+      lat = posicion.coords.latitude;
+
+      //ubicacion actual
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=`;
+
+      //ubicacion por ciudad
+      //const url = "https://api.openweathermap.org/data/2.5/weather?q=Merida,mx&callback=test&appid=95bef9bb448ce3cea02ada399cc278e8"
+
+      //console.log(url)
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+
+          let temp = Math.round(data.main.temp);
+          temperaturaValor.textContent = `${temp} °C`;
+
+          let desc = data.weather[0].description;
+          temperaturaDescripcion.textContent = desc.toUpperCase();
+
+          ubicacion.textContent = data.name;
+
+          vientoVelocidad.textContent = `${data.wind.speed} m/s`;
+          console.log(data.name);
+
+          //iconos estaticos
+          /*
+                console.log(data.weather[0].icon)
+                let iconCode = data.weather[0].icon
+                const urlIcon = `https://openweathermap.org/img/wn/${iconCode}.png`
+                console.log(urlIcon)
+                */
+
+          //para iconos dinámicos
+          console.log(data.weather[0].main);
+          switch (data.weather[0].main) {
+            case "Thunderstorm":
+              iconoAnimado.src = "/animated/thunder.svg";
+              console.log("TORMENTA");
+              break;
+            case "Drizzle":
+              iconoAnimado.src = "/animated/rainy-2.svg";
+              console.log("LLOVIZNA");
+              break;
+            case "Rain":
+              iconoAnimado.src = "/animated/rainy-7.svg";
+              console.log("LLUVIA");
+              break;
+            case "Snow":
+              iconoAnimado.src = "/animated/snowy-6.svg";
+              console.log("NIEVE");
+              break;
+            case "Clear":
+              iconoAnimado.src = "/animated/day.svg";
+              console.log("LIMPIO");
+              break;
+            case "Atmosphere":
+              iconoAnimado.src = "/animated/weather.svg";
+              console.log("ATMOSFERA");
+              break;
+            case "Clouds":
+              iconoAnimado.src = "/animated/cloudy-day-1.svg";
+              console.log("NUBES");
+              break;
+            default:
+              iconoAnimado.src = "/animated/cloudy-day-1.svg";
+              console.log("por defecto");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  }
+});
